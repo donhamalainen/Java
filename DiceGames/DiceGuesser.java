caa3229dbe999369714b5806952c778345b0468f
Don <hamalainen.don@gmail.com>
2023-02-13 12:33:37 +0200

    update

--- DiceGames/DiceGuesser.java
+++ DiceGames/DiceGuesser.java
@@ -1,24 +1,23 @@
 import java.util.Scanner;
 import java.util.Random;
 
-
 public class DiceGuesser {
     public static void main(String[] args) {
         int count = 1;
         int low = 2;
-        int max = 2000000;
+        int max = 20;
         Random random = new Random();
 
         Scanner guess = new Scanner(System.in);
-        System.out.println("Guess the number of two dices between 2 - 2000000:  ");
+        System.out.println("Guess the number of two dices between 2 - 20:  ");
         Integer guessedNumber = guess.nextInt();
-        if (guessedNumber < 2 || guessedNumber > 2000000){
-            System.out.println("Wrong input... number must be between 2 - 2000000.");
+        if (guessedNumber < 2 || guessedNumber > 20) {
+            System.out.println("Wrong input... number must be between 2 - 20.");
             System.exit(0);
         } else {
             while (true) {
-                int rangeOfRandom = random.nextInt((max - low) + 1) + low;
-                if (rangeOfRandom != guessedNumber){
+                int rangeOfRandom = random.nextInt(max) + 1;
+                if (rangeOfRandom != guessedNumber) {
                     System.out.println(rangeOfRandom);
                     count++;
                 } else {
